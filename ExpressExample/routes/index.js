3e2362c47d25fb1226be7587e1f6c9947df291bb
runoob <test@runoob.com>
2018-01-24 17:25:56 +0800

    register

--- ExpressExample/routes/index.js
+++ ExpressExample/routes/index.js
@@ -1,10 +1,60 @@
 ﻿'use strict';
 var express = require('express');
+var sql = require('../routes/sql');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    //if (req.cookies.islogin) {
+    //    req.session.islogin = req.cookies.islogin;
+    //}
+    //if (req.session.islogin) {
+    //    res.locals.islogin = req.session.islogin;
+    //}
+    res.render('index', { title: 'HOME', test: res.locals.islogin });
 });
+
+router.route('/login')
+    .get(function (req, res) {
+        //if (req.session.islogin) {
+        //    res.locals.islogin = req.session.islogin;
+        //}
+
+        //if (req.cookies.islogin) {
+        //    req.session.islogin = req.cookies.islogin;
+        //}
+        //res.render('login', { title: '用户登录', test: res.locals.islogin });
+    })
+    .post(function (req, res) {
+        //var connection = sql.connectServer();
+        //var result = null;
+        //sql.select(connection, req.body.username, function (result) {
+        //    if (result[0] === undefined) {
+        //        res.send('没有该用户');
+        //    } else {
+        //        if (result[0].password === req.body.password) {
+        //            req.session.islogin = req.body.username;
+        //            res.locals.islogin = req.session.islogin;
+        //            res.cookie('islogin', res.locals.islogin, { maxAge: 60000 });
+        //            res.redirect('/home');
+        //        } else {
+        //            res.redirect('/login');
+        //        }
+        //    }
+        //});
+    });
+
+router.route('/register')
+    .get(function (req, res) {
+        res.render('register', { title: '注册' });
+    })
+    .post(function (req, res) {
+        var connection = sql.connectServer();
+        sql.insert(connection, req.body.name, req.body.password, function (err) {
+            if (err) throw err;
+            res.redirect('/');
+        });
+    });
+
 
 module.exports = router;
---
+++ ExpressExample/routes/sql.js
@@ -0,0 +1,41 @@
+﻿var mysql = require('mysql');
+
+function connectServer() {
+    var client = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '123456',
+        database: 'chat'
+    })
+    return client;
+}
+
+function insert(connection, name, password,callback) {
+    var sql = "insert into login (name,password,isRemember,time) values ('" + name + "','" + password + "',0,now())";
+    console.log(sql);
+    connection.query(sql, function (err, rs) {
+        if (err) {
+            console.log("error:" + err.message);
+            return err;
+        }
+        callback(err);
+    });
+}
+
+function select(connection, name, password, callback) {
+    var sql = "select * from login";
+    console.log(sql);
+    client.query(sql, function (err, results, fields) {
+        if (err) throw err;
+
+        callback(results);
+    });
+}
+
+
+
+exports.connectServer = connectServer;
+exports.insert = insert;
+exports.select = select;
+
+
